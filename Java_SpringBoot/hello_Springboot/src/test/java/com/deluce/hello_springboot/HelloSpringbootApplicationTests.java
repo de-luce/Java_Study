de02b5e24b65a4702af04732c4677e3e43564044
de-luce <1934256825@qq.com>
2024-03-07 21:28:05 +0800

    Initial commit

---
+++ Java_SpringBoot/hello_Springboot/src/test/java/com/deluce/hello_springboot/HelloSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deluce.hello_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
