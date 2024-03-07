27f6a9484258dc40a7176f0a8cc0b82e9d3fb4d9
de-luce <1934256825@qq.com>
2024-03-07 21:08:09 +0800

    Initial commit

---
+++ Java_SpringBoot/SpringBoot_01_FirstBoot/src/main/java/com/deluce/springboot_01_firstboot/SpringBoot01FirstBootApplication.java
@@ -0,0 +1,13 @@
+package com.deluce.springboot_01_firstboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01FirstBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01FirstBootApplication.class, args);
+    }
+
+}
---
+++ Java_SpringBoot/SpringBoot_01_FirstBoot/src/test/java/com/deluce/springboot_01_firstboot/SpringBoot01FirstBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deluce.springboot_01_firstboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot01FirstBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
